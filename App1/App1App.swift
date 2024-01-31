193ecf467ba19df3453ad6bdd5cfe40414fd005b
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-01-31 13:03:46 -0500

    Add Project Files

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Tarasha Bansal on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Tarasha Bansal on 1/23/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Tarasha!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
