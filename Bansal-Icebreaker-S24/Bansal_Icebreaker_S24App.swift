c17d2e5a3628d52cd86e9899f4fdb951542b2634
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-02-13 21:01:37 -0500

    Initial Commit

---
+++ Bansal-Icebreaker-S24/Bansal_Icebreaker_S24App.swift
@@ -0,0 +1,17 @@
+//
+//  Bansal_Icebreaker_S24App.swift
+//  Bansal-Icebreaker-S24
+//
+//  Created by Tarasha Bansal on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Bansal_Icebreaker_S24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bansal-Icebreaker-S24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bansal-Icebreaker-S24
+//
+//  Created by Tarasha Bansal on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
